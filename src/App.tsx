2467c300654aaf952bd74cb37bcbbf71799e0eec
Rafael Martinez <rafaelmartinez011@gmail.com>
2023-07-26 20:27:14 -0400

    removing tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-function App (): JSX.Element {
-  return (
-    <>
+import { Component } from 'react'
 
-    </>
-  )
+class App extends Component {
+  render (): JSX.Element {
+    return (
+      <h1 className='text-red-400 font-medium'> Hello World! </h1>
+    )
+  }
 }
 
 export default App
